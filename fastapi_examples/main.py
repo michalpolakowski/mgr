cf01f37b630734aaeff7a9d3695aaa0945cea0f0
Michał Polakowski <michal.polakowski@profil-software.com>
2021-06-03 18:03:09 +0200

    adds threading and processing examples for FastAPI

--- fastapi_examples/main.py
+++ fastapi_examples/main.py
@@ -1,7 +1,18 @@
 import asyncio
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
 
+import requests
 from fastapi import FastAPI
 import aiohttp
+from fastapi.logger import logger
+
+
+NUMBER_OF_REQUEST = 1000
+
+URL_TO_BE_REQUESTED = 'http://127.0.0.1:8000/'
+
+aio_logger = logger
+
 
 app = FastAPI()
 
@@ -11,18 +22,42 @@ def home():
     return {'welcome_text': 'Welcome in FastAPI showcase project'}
 
 
-@app.get('/fetch-sites')
-async def fetch_sites():
-    url = "https://www.google.com"
+@app.get('/fetch-sites-async')
+async def fetch_sites_asyncio():
     tasks = []
     async with aiohttp.ClientSession() as session:
-        for i in range(8):
-            task = asyncio.ensure_future(get_site(url, session))
+        for i in range(NUMBER_OF_REQUEST):
+            task = asyncio.ensure_future(get_site(URL_TO_BE_REQUESTED, session))
             tasks.append(task)
-        responses = await asyncio.gather(*tasks)
-        return responses
+        return await asyncio.gather(*tasks)
+
+
+@app.get('/fetch-sites-threading')
+def fetch_sites_threading():
+    with ThreadPoolExecutor() as ex:
+        res = ex.map(get_response_body, (URL_TO_BE_REQUESTED for _ in range(NUMBER_OF_REQUEST)))
+    return res
+
+
+@app.get('/fetch-sites-processing')
+def fetch_sites_processing():
+    with ProcessPoolExecutor() as ex:
+        res = ex.map(get_response_body, (URL_TO_BE_REQUESTED for _ in range(NUMBER_OF_REQUEST)))
+    return res
+
+
+@app.get('/fetch-sites-sync')
+def fetch_sites_sync():
+    responses = []
+    for i in range(NUMBER_OF_REQUEST):
+        responses.append(requests.get(URL_TO_BE_REQUESTED).content)
+    return responses
 
 
 async def get_site(url: str, session: aiohttp.ClientSession):
     async with session.get(url) as response:
         return await response.read()
+
+
+def get_response_body(url):
+    return requests.get(url).content
