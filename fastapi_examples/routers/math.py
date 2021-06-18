d3620fbd960379b7ee1105df562b37bbb23b4107
Michał Polakowski <michal.polakowski@profil-software.com>
2021-06-18 21:20:41 +0200

    adds requirements for django and fastapi

--- fastapi_examples/routers/math.py
+++ fastapi_examples/routers/math.py
@@ -13,23 +13,23 @@ router = APIRouter()
 @router.get('/count-sync')
 async def count_sync() -> Dict[str, int]:
     res = [cpu_heavy(i) for i in range(COUNTS_RANGE)]
-    return {'result': sum(res)}
+    return sum(res)
 
 
 @router.get('/count-threading')
 async def count_threading() -> Dict[str, int]:
     with ThreadPoolExecutor() as ex:
         res = ex.map(cpu_heavy, range(COUNTS_RANGE))
-    return {'result': sum(res)}
+    return sum(res)
 
 
 @router.get('/count-processing')
 async def count_processing() -> Dict[str, int]:
     with ProcessPoolExecutor() as ex:
         res = ex.map(cpu_heavy, range(COUNTS_RANGE))
-    return {'result': sum(res)}
+    return sum(res)
 
 
 @router.get('/count-async')
 async def count_async() -> Dict[str, int]:
-    return {'result': sum(await asyncio.gather(*[acpu_heavy(i) for i in range(COUNTS_RANGE)]))}
+    return sum(await asyncio.gather(*[acpu_heavy(i) for i in range(COUNTS_RANGE)]))
